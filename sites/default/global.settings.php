ae3480a3bef5699bbba3a489dd86c8cb075c03c9
Chris Shattuck <hello@buildamodule.com>
2011-09-26 13:35:22 -0600

    Added string overrides to global.settings.php file

--- sites/default/global.settings.php
+++ sites/default/global.settings.php
@@ -11,4 +11,6 @@
  */
  $conf['locale_custom_strings_en'][''] = array(
    'Navigation'      => 'Site Menu',
+   'Submitted by !username on !datetime' => 'Written by the amazing !username around !datetime',
+   'Powered by <a href="@poweredby">Drupal</a>' => 'What rhymes with <a href="@poweredby">Drupal</a>?',
  );
