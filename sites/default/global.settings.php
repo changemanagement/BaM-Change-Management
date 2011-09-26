f8c03dcbec1e9b6342e67f5a4e48d550078f5105
Chris Shattuck <hello@buildamodule.com>
2011-09-26 12:27:26 -0600

    Added global.settings.php file

---
+++ sites/default/global.settings.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * String overrides:
+ *
+ * To override specific strings on your site with or without enabling locale
+ * module, add an entry to this list. This functionality allows you to change
+ * a small number of your site's default English language interface strings.
+ *
+ * Remove the leading hash signs to enable.
+ */
+ $conf['locale_custom_strings_en'][''] = array(
+   'Navigation'      => 'Site Menu',
+ );
